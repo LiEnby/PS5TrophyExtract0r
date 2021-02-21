3fa0eb856126457921f9576ac572efd6d255285d
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2021-02-21 17:31:59 +1300

    Add files via upload

---
+++ PS5TrophyExtract/TrophyReader.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace PS5TrophyExtract
+{
+    partial class TrophyReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrophyReader));
+            this.previewBox = new System.Windows.Forms.PictureBox();
+            this.openTrophys = new System.Windows.Forms.Button();
+            this.trophyDataFiles = new System.Windows.Forms.ListBox();
+            this.extractFile = new System.Windows.Forms.Button();
+            this.extractAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // previewBox
+            // 
+            this.previewBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.previewBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.previewBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("previewBox.ErrorImage")));
+            this.previewBox.Location = new System.Drawing.Point(12, 12);
+            this.previewBox.Name = "previewBox";
+            this.previewBox.Size = new System.Drawing.Size(510, 473);
+            this.previewBox.TabIndex = 1;
+            this.previewBox.TabStop = false;
+            // 
+            // openTrophys
+            // 
+            this.openTrophys.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.openTrophys.Location = new System.Drawing.Point(12, 694);
+            this.openTrophys.Name = "openTrophys";
+            this.openTrophys.Size = new System.Drawing.Size(510, 23);
+            this.openTrophys.TabIndex = 2;
+            this.openTrophys.Text = "Open trophy00.ucp";
+            this.openTrophys.UseVisualStyleBackColor = true;
+            this.openTrophys.Click += new System.EventHandler(this.openTrophys_Click);
+            // 
+            // trophyDataFiles
+            // 
+            this.trophyDataFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trophyDataFiles.FormattingEnabled = true;
+            this.trophyDataFiles.Location = new System.Drawing.Point(12, 491);
+            this.trophyDataFiles.Name = "trophyDataFiles";
+            this.trophyDataFiles.Size = new System.Drawing.Size(510, 134);
+            this.trophyDataFiles.TabIndex = 3;
+            this.trophyDataFiles.SelectedIndexChanged += new System.EventHandler(this.trophyDataFiles_SelectedIndexChanged);
+            // 
+            // extractFile
+            // 
+            this.extractFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.extractFile.Enabled = false;
+            this.extractFile.Location = new System.Drawing.Point(12, 636);
+            this.extractFile.Name = "extractFile";
+            this.extractFile.Size = new System.Drawing.Size(510, 23);
+            this.extractFile.TabIndex = 4;
+            this.extractFile.Text = "Extract File";
+            this.extractFile.UseVisualStyleBackColor = true;
+            this.extractFile.Click += new System.EventHandler(this.extractFile_Click);
+            // 
+            // extractAll
+            // 
+            this.extractAll.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.extractAll.Enabled = false;
+            this.extractAll.Location = new System.Drawing.Point(12, 665);
+            this.extractAll.Name = "extractAll";
+            this.extractAll.Size = new System.Drawing.Size(510, 23);
+            this.extractAll.TabIndex = 5;
+            this.extractAll.Text = "Extract All Files";
+            this.extractAll.UseVisualStyleBackColor = true;
+            this.extractAll.Click += new System.EventHandler(this.extractAll_Click);
+            // 
+            // TrophyReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 723);
+            this.Controls.Add(this.extractAll);
+            this.Controls.Add(this.extractFile);
+            this.Controls.Add(this.trophyDataFiles);
+            this.Controls.Add(this.openTrophys);
+            this.Controls.Add(this.previewBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TrophyReader";
+            this.Text = "Trophy Data Reader";
+            ((System.ComponentModel.ISupportInitialize)(this.previewBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox previewBox;
+        private System.Windows.Forms.Button openTrophys;
+        private System.Windows.Forms.ListBox trophyDataFiles;
+        private System.Windows.Forms.Button extractFile;
+        private System.Windows.Forms.Button extractAll;
+    }
+}
+
---
+++ PS5TrophyExtract/TrophyReader.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS5TrophyExtract
+{
+    public partial class TrophyReader : Form
+    {
+        public TrophyReader()
+        {
+            InitializeComponent();
+        }
+
+        public List<TableOfContents> TOC = new List<TableOfContents>();
+        public class TableOfContents
+        {
+            public TableOfContents(int offset, int length, string filename)
+            {
+                Offset = offset;
+                Length = length;
+                Filename = filename;
+            }
+
+            public int Offset;
+            public int Length;
+            public string Filename;
+            
+        }
+
+        private Stream data;
+
+        private string readCStr()
+        {
+            string str = "";
+            while(true)
+            {
+                byte readByte = (byte)data.ReadByte();
+                if (readByte != 0)
+                    str += (char)readByte;
+                else
+                    break;
+            }
+            return str;
+        }
+        private int readInt32()
+        {
+            byte[] intBytes = new byte[4];
+            data.Read(intBytes, 0x00, intBytes.Length);
+            return BitConverter.ToInt32(intBytes, 0);
+        }
+        private int readInt32BE()
+        {
+            byte[] intBytes = new byte[4];
+            data.Read(intBytes, 0x00, intBytes.Length);
+            intBytes = intBytes.Reverse().ToArray();
+            return BitConverter.ToInt32(intBytes, 0);
+        }
+        private void readToc(string filename)
+        {
+            if (data != null)
+                data.Dispose();
+
+            FileStream fs = File.OpenRead(filename);
+            data = fs;
+
+            fs.Seek(0x13, SeekOrigin.Begin);
+            int totalEntries = readInt32();
+            int tocLocation = readInt32();
+
+            fs.Seek(tocLocation, SeekOrigin.Begin);
+            fs.Seek(0x10, SeekOrigin.Current);
+
+            // Read toc
+            for (int i = 0; i < totalEntries; i++)
+            {
+                fs.Seek(0x10, SeekOrigin.Current);
+                string fileName = readCStr();
+                fs.Seek(0x23 - fileName.Length, SeekOrigin.Current);
+                int fileLocation = readInt32BE();
+                fs.Seek(0x4, SeekOrigin.Current);
+                int fileEnd = readInt32BE();
+
+                TableOfContents toc = new TableOfContents(fileLocation, fileEnd, fileName);
+                TOC.Add(toc);
+            }
+
+        }
+
+        private byte[] readFile(TableOfContents tocEntry)
+        {
+            byte[] fileData = new byte[tocEntry.Length];
+            data.Seek(tocEntry.Offset, SeekOrigin.Begin);
+            data.Read(fileData, 0x00, tocEntry.Length);
+            return fileData;
+        }
+
+        private void populateList()
+        {
+            trophyDataFiles.Items.Clear();
+
+            foreach(TableOfContents contents in TOC)
+            {
+                trophyDataFiles.Items.Add(contents.Filename);
+            }
+
+            extractFile.Enabled = false;
+            extractAll.Enabled = true;
+        }
+
+
+        private void openTrophys_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Trophy Set Files (*.ucp)|*.ucp";
+            DialogResult res = fileDialog.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                string filePath = fileDialog.FileName;
+                readToc(filePath);
+                populateList();
+                previewBox.BackgroundImage = null;
+            }
+
+        }
+
+        private void trophyDataFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                previewBox.BackgroundImage = Bitmap.FromStream(new MemoryStream(readFile(TOC[trophyDataFiles.SelectedIndex])));
+            }
+            catch (Exception) {
+                previewBox.BackgroundImage = previewBox.ErrorImage;
+            };
+
+            if (trophyDataFiles.SelectedIndex >= 0 && trophyDataFiles.SelectedIndex < TOC.Count)
+            {
+                extractFile.Enabled = true;
+            }    
+
+        }
+
+        private void extractFile_Click(object sender, EventArgs e)
+        {
+            if (!(trophyDataFiles.SelectedIndex >= 0 && trophyDataFiles.SelectedIndex < TOC.Count))
+                return;
+                
+            TableOfContents file = TOC[trophyDataFiles.SelectedIndex];
+
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.FileName = file.Filename;
+            fileDialog.Title = "Save file";
+            DialogResult res = fileDialog.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                File.WriteAllBytes(fileDialog.FileName, readFile(file));
+                MessageBox.Show("Saved file to " + fileDialog.FileName, "Saved!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void extractAll_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.Description = "Output folder";
+            DialogResult res = folderDialog.ShowDialog();
+            if(res == DialogResult.OK)
+            {
+                foreach(TableOfContents content in TOC)
+                {
+                    string newFile = Path.Combine(folderDialog.SelectedPath, content.Filename);
+                    File.WriteAllBytes(newFile, readFile(content));
+                }
+                MessageBox.Show("Saved all files to " + folderDialog.SelectedPath, "Saved!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
